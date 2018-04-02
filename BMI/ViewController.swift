05ccb188b5ea95b3911ec56a5c5b75e6a0cebb90
Masashi-Takata <t.masashi.1218@icloud.com>
2018-04-02 12:32:26 +0900

    up date

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController,UITextFieldDelegate {
     
@@ -25,11 +26,22 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var correctWeightText: UILabel!
     @IBOutlet weak var talentImage: UIImageView!
     @IBOutlet weak var talentText: UILabel!
+    var yabeImage = UIImage(named:"yabe")!
     var kiritaniImage = UIImage(named:"kiritani")!
     var kitagawaImage = UIImage(named:"kitagawa")!
     var amuroImage = UIImage(named:"amuro")!
     var hukiisiImage = UIImage(named:"hukiisi")!
     var ayaseImage = UIImage(named:"ayase")!
+    var nishikawaImage = UIImage(named:"nishikawa")!
+    var ooshimaImage = UIImage(named:"ooshima")!
+    var wakkimaImage = UIImage(named:"wakki")!
+    var watanabeImage = UIImage(named:"watanabe")!
+    var kurosawaImage = UIImage(named:"kurosawa")!
+    var yashiroImage = UIImage(named:"yashiro")!
+    var matukoImage = UIImage(named:"matuko")!
+    var hikomaroImage = UIImage(named:"hikomaro")!
+    var ishizukaImage = UIImage(named:"ishizuka")!
+    var kennkobaImage = UIImage(named:"kennkoba")!
     
     @IBAction func calculation(_ sender: Any) {
         
@@ -40,49 +52,97 @@ class ViewController: UIViewController,UITextFieldDelegate {
         let bmi = weight / (height/100) / (height/100)
         
         let weightResult = (height/100) * (height/100) * Double(bmi_coefficient)
+        let weightDifference = weight-weightResult
         
         if (Double(bmi) < 18.5) {
             bodyStyleResultText.text = "あなたはやせ体型です"
             bodyStyleResultText.textColor = UIColor.blue
             if (Double(bmi) < 14.51 && genderSelect.selectedSegmentIndex == 0) {
-            talentImage.image = kiritaniImage
-            talentText.text = "あなたの体型は桐谷美玲です"
+            talentImage.image = yabeImage
+            talentText.text = "あなたの体型はカラテカ矢部です"
             } else if(Double(bmi) < 14.51 && genderSelect.selectedSegmentIndex == 1) {
                 talentImage.image = kiritaniImage
                 talentText.text = "あなたの体型は桐谷美玲です"
+            } else if (Double(bmi) > 14.51 && Double(bmi) <= 15.51 && genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = yabeImage
+                talentText.text = "あなたの体型はカラテカ矢部です"
             } else if (Double(bmi) > 14.51 && Double(bmi) <= 15.51 && genderSelect.selectedSegmentIndex == 1) {
                 talentImage.image = kitagawaImage
                 talentText.text = "あなたの体型は北川景子です"
+            } else if (Double(bmi) > 15.51 && Double(bmi) <= 16.51 && genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = yabeImage
+                talentText.text = "あなたの体型はカラテカ矢部です"
             } else if (Double(bmi) > 15.51 && Double(bmi) <= 16.51 && genderSelect.selectedSegmentIndex == 1) {
             talentImage.image = amuroImage
             talentText.text = "あなたの体型は安室奈美恵です"
+            } else if (Double(bmi) > 16.51 && Double(bmi) <= 17.51 && genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = yabeImage
+                talentText.text = "あなたの体型はカラテカ矢部です"
             } else if (Double(bmi) > 16.51 && Double(bmi) <= 17.51 && genderSelect.selectedSegmentIndex == 1) {
                 talentImage.image = hukiisiImage
                 talentText.text = "あなたの体型は吹石一恵です"
             } else if (Double(bmi) > 17.51 && genderSelect.selectedSegmentIndex == 1) {
                 talentImage.image = ayaseImage
                 talentText.text = "あなたの体型は綾瀬はるかです"
+            } else if (Double(bmi) > 17.51 && genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = nishikawaImage
+                talentText.text = "あなたの体型は西川貴教です"
             }
         } else if (18.5 <= Double(bmi) && Double(bmi) < 25){
             bodyStyleResultText.text = "あなたは標準体型です"
             bodyStyleResultText.textColor = UIColor.green
+            if (genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = wakkimaImage
+                talentText.text = "あなたの体型はペナルティーワッキーです"
+            } else if(genderSelect.selectedSegmentIndex == 1) {
+                talentImage.image = ooshimaImage
+                talentText.text = "あなたの体型は大島優子です"
+            }
         } else if (25 <= Double(bmi) && Double(bmi) < 30){
             bodyStyleResultText.text = "あなたは肥満度１体型です"
             bodyStyleResultText.textColor = UIColor.magenta
+            if (genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = kennkobaImage
+                talentText.text = "あなたの体型はケンドーコバヤシです"
+            } else if(genderSelect.selectedSegmentIndex == 1) {
+                talentImage.image = kurosawaImage
+                talentText.text = "あなたの体型は黒澤かずこです"
+            }
         } else if (30 <= Double(bmi) && Double(bmi) < 35){
             bodyStyleResultText.text = "あなたは肥満度2体型です"
             bodyStyleResultText.textColor = UIColor.yellow
+            if (genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = hikomaroImage
+                talentText.text = "あなたの体型は彦摩呂です"
+            } else if(genderSelect.selectedSegmentIndex == 1) {
+                talentImage.image = yashiroImage
+                talentText.text = "あなたの体型はやしろ優です"
+            }
         } else if (35 <= Double(bmi) && Double(bmi) < 40){
             bodyStyleResultText.text = "あなたは肥満度3体型です"
             bodyStyleResultText.textColor = UIColor.orange
+            if (genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = ishizukaImage
+                talentText.text = "あなたの体型は石塚英彦です"
+            } else if(genderSelect.selectedSegmentIndex == 1) {
+                talentImage.image = watanabeImage
+                talentText.text = "あなたの体型は渡辺直美です"
+            }
         } else if (40 <= Double(bmi)){
             bodyStyleResultText.text = "あなたは肥満度4体型です"
             bodyStyleResultText.textColor = UIColor.red
+            if (genderSelect.selectedSegmentIndex == 0) {
+                talentImage.image = matukoImage
+                talentText.text = "あなたの体型はマツコデラックスです"
+            } else if(genderSelect.selectedSegmentIndex == 1) {
+                talentImage.image = matukoImage
+                talentText.text = "あなたの体型はマツコデラックスです"
+            }
         }
         
         yourBMIResultText.text = String(format: "%.2f", bmi)  //小数点指定
         
-        correctWeightText.text = String(format: "%.2f", weightResult)
+        correctWeightText.text = String(format: "%.2f(適正体重との差 %.2fkg)", weightResult,weightDifference)
         view.endEditing(true)  //キーボードを非表示
     }
     
@@ -100,6 +160,32 @@ class ViewController: UIViewController,UITextFieldDelegate {
            bmi_coefficient = 22  //デフォルト
         }
     }
+    
+    @IBAction func shareTwitter(_ sender: Any) {
+        let shareText = talentText.text
+        let shareImage = talentImage.image
+        
+        let shareItems = [shareText,"#NAIST","#ユビ研",shareImage] as [Any]
+        
+        let avc = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+        
+        // 使用しないアクティビティタイプ
+        let excludedActivityTypes = [
+            UIActivityType.postToWeibo,
+            UIActivityType.saveToCameraRoll,
+            UIActivityType.print,
+            UIActivityType.copyToPasteboard,
+            UIActivityType.airDrop,
+            UIActivityType.assignToContact,
+            UIActivityType.addToReadingList,
+            UIActivityType.mail,
+            UIActivityType.message
+        ]
+        avc.excludedActivityTypes = excludedActivityTypes
+        
+        present(avc, animated: true, completion: nil)
+    }
+    
     
     
     override func viewDidLoad() {
